e5ec6dd4fd3dc5f120607b80be03f70c69be18ed
Anton Ulyanov <anton.ulianov@gmail.com>
2019-10-01 11:32:35 +0700

    400-fix-compile-with-musl

--- src/system.h
+++ src/system.h
@@ -118,6 +118,11 @@
 #include <signal.h>
 #endif
 
+#ifdef HAVE_SYS_SYSINFO_H
+#define _LINUX_SYSINFO_H
+#include <sys/sysinfo.h>
+#endif
+
 #if defined(__linux__)
 #include <asm/types.h>
 #include <linux/if.h>
@@ -159,10 +164,6 @@
 
 #ifdef HAVE_NET_IF_TUN_H
 #include <net/if_tun.h>
-#endif
-
-#ifdef HAVE_NET_ETHERNET_H
-#include <net/ethernet.h>
 #endif
 
 #ifdef HAVE_ASM_TYPES_H
